fb9e4543f8561e8feb44446631baa8e0abee5bce
IDeadInside-1000-7 <90912641+IDeadInside-1000-7@users.noreply.github.com>
2021-10-12 14:55:49 +0300

    Update Source.cpp

--- lab_vvp_8-(1)/Source.cpp
+++ lab_vvp_8-(1)/Source.cpp
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-void main()
+int main()
 {
-	int Bt, kBt;
+	float Bt, kBt;
 	printf("Enter your count of Bytes, then programm will convert it to KiloBytes ");
 	printf("Bt = ");
-	scanf("%i", &Bt);
+	scanf("%f", &Bt);
 	printf("In KiloBytes = ");
 	kBt = Bt / 1024;
-	printf("%.0i\n", kBt);
+	kBt = ceil(kBt);
+	printf("%f", kBt);
 }
